5b07006fdc1ca3f8ef8723e64a5f6f6d585e29ef
Hector <Hector.Mojica0263@gmail.com>
2024-12-05 20:01:25 -0800

    Fixed dark theme bug
    Fixed dark theme bug where dark theme would not be applied to the side menu

--- Park Patrol/SideMenu.swift
+++ Park Patrol/SideMenu.swift
@@ -32,6 +32,7 @@ struct SideMenu: View {
     @Binding var selectedOption: MenuOption?
     /// Environment variable for view dismissal
     @Environment(\.presentationMode) var presentationMode
+    @Environment(\.colorScheme) var colorScheme
 
     // MARK: - View Body
     var body: some View {
@@ -47,11 +48,11 @@ struct SideMenu: View {
                 }) {
                     Text(option.rawValue)
                         .font(.headline)
-                        .foregroundColor(Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)))
+                        .foregroundColor(Color.primary)
                         .padding(.vertical, 12)
                         .padding(.horizontal, 24)
                         .frame(maxWidth: .infinity, alignment: .leading)
-                        .background(Color(#colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.968627451, alpha: 1)))
+                        .background(Color(.secondarySystemBackground))
                         .cornerRadius(8)
                 }
             }
@@ -61,7 +62,9 @@ struct SideMenu: View {
         // Menu styling and animation
         .padding()
         .frame(width: 250)
-        .background(Color.white)
+        .background(
+            colorScheme == .dark ? Color.black : Color.white
+        )
         .shadow(color: Color.black.opacity(0.2), radius: 5, x: 2, y: 0)
         .transition(.move(edge: .leading).combined(with: .opacity))
     }
